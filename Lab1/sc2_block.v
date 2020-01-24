498b93c98623be2d733f4659c99e97b551581fd1
Birkhoff <zhiqic2@illinois.edu>
2020-01-24 00:38:11 -0600

    Add sc2_block

--- Lab1/sc2_block.v
+++ Lab1/sc2_block.v
@@ -3,5 +3,11 @@
 // Refer to sc_block.v for hints!
 
 module sc2_block(s, cout, a, b, cin);
+	output s, cout;
+	input a, b, cin;
+	wire w0, w1, w2;
 
+	or o1(cout, w2, w1);
+	sc_block sb0(w0, w1, a, b);
+	sc_block sb1(s, w2, w0, cin);
 endmodule // sc2_block
