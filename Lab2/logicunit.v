8d822632e5334ea6ff820965b78aef4175d661a9
Birkhoff <zhiqic2@illinois.edu>
2020-02-01 15:31:25 -0600

    Add logicunit

--- Lab2/logicunit.v
+++ Lab2/logicunit.v
@@ -1,7 +1,12 @@
 // 00 -> AND, 01 -> OR, 10 -> NOR, 11 -> XOR
 module logicunit(out, A, B, control);
-    output      out;
-    input       A, B;
-    input [1:0] control;
-
+	output out;
+	input A, B;
+	input [1:0] control;
+	wire a_and_b, a_or_b, a_nor_b, a_xor_b;
+	and	a1(a_and_b, A, B);
+	or o1(a_or_b, A, B);
+	nor n1(a_nor_b, A, B);
+	xor x1(a_xor_b, A, B);
+	mux4 m1(out, a_and_b, a_or_b, a_nor_b, a_xor_b, control);
 endmodule // logicunit
--- Lab2/logicunit_tb.v
+++ Lab2/logicunit_tb.v
@@ -1,3 +1,25 @@
 module logicunit_test;
-    // exhaustively test your logic unit implementation by adapting mux4_tb.v
+	reg A = 0;
+	always #1 A = !A;
+	reg B = 0;
+	always #2 B = !B;
+	reg [1:0] control = 0;
+	 
+	initial begin
+		$dumpfile("logicunit.vcd");
+		$dumpvars(0, logicunit_test);
+
+		# 16 control = 1;
+		# 16 control = 2;
+		# 16 control = 3;
+		# 16 $finish;
+	end
+
+	wire out;
+	logicunit l1(out, A, B, control);
+
+	initial begin
+		$display("A B control out");
+		$monitor("%d %d %d %d (at time %t)", A, B, control, out, $time);
+	end
 endmodule // logicunit_test
