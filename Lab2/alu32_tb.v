b8008a2f75e470a840d4a624df46a41a3256f917
Birkhoff <zhiqic2@illinois.edu>
2020-02-03 16:37:20 -0600

    Add more test cases

--- Lab2/alu32_tb.v
+++ Lab2/alu32_tb.v
@@ -8,9 +8,14 @@ module alu32_test;
 		$dumpvars(0, alu32_test);
 
 			 A = 8; B = 4; control = `ALU_ADD; // try adding 8 and 4
+		# 10 A = 2147483647; B = 1; control = `ALU_ADD; // should have overflow
+		# 10 A = 2147483647; B = 1; control = `ALU_UADD; // shouldn't have overflow
 		# 10 A = 2; B = 5; control = `ALU_SUB; // try subtracting 5 from 2
-		// add more test cases here!
-
+		# 10 A = 2; B = 2; control = `ALU_SUB; // should be 0
+		# 10 A = 19720; B = 29132; control = `ALU_OR; // should be 32204
+		# 10 A = 14419; B = 31601; control = `ALU_AND; // should be 14417
+		# 10 A = 17186; B = 31601; control = `ALU_XOR; // should be 14419
+		# 10 A = 26219; B = 21154; control = `ALU_NOR; // should be -30444
 		# 10 $finish;
 	end
 
@@ -22,7 +27,7 @@ module alu32_test;
 	always @(out) myint = out;
 	
 	initial begin
-		$display("\tA\t\tB\tcontrol\t\tint\t\tout\toverflow\tzero\tnegative");
+		$display("\t A\t\t B\tcontrol\t\tint\t\tout\toverflow\tzero\tnegative");
 		$monitor("%d\t%d\t%d\t%d\t%d\t%d\t\t%d\t%d\t(at time %t)", A, B, control, myint, out, overflow, zero, negative, $time);
 	end
 endmodule // alu32_test
