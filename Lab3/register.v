3e84132113190dda6e8fa91f39d1e7a529c83391
Birkhoff <zhiqic2@illinois.edu>
2020-02-04 22:12:16 -0600

    Add register

--- Lab3/register.v
+++ Lab3/register.v
@@ -1,43 +1,71 @@
 // register: A register which may be reset to an arbirary value
 //
-// q      (output) - Current value of register
-// d      (input)  - Next value of register
-// clk    (input)  - Clock (positive edge-sensitive)
+// q	  (output) - Current value of register
+// d	  (input)  - Next value of register
+// clk	(input)  - Clock (positive edge-sensitive)
 // enable (input)  - Load new value? (yes = 1, no = 0)
-// reset  (input)  - Asynchronous reset    (reset = 1)
+// reset  (input)  - Asynchronous reset	(reset = 1)
 //
 module register(q, d, clk, enable, reset);
-
-    output [31:0] q;
-    input  [31:0] d;
-    input  clk, enable, reset;
-
-   //  Your implementation goes here
-
+	output [31:0] q;
+	input  [31:0] d;
+	input  clk, enable, reset;
+	dffe d0(q[0], d[0], clk, enable, reset);
+	dffe d1(q[1], d[1], clk, enable, reset);
+	dffe d2(q[2], d[2], clk, enable, reset);
+	dffe d3(q[3], d[3], clk, enable, reset);
+	dffe d4(q[4], d[4], clk, enable, reset);
+	dffe d5(q[5], d[5], clk, enable, reset);
+	dffe d6(q[6], d[6], clk, enable, reset);
+	dffe d7(q[7], d[7], clk, enable, reset);
+	dffe d8(q[8], d[8], clk, enable, reset);
+	dffe d9(q[9], d[9], clk, enable, reset);
+	dffe d10(q[10], d[10], clk, enable, reset);
+	dffe d11(q[11], d[11], clk, enable, reset);
+	dffe d12(q[12], d[12], clk, enable, reset);
+	dffe d13(q[13], d[13], clk, enable, reset);
+	dffe d14(q[14], d[14], clk, enable, reset);
+	dffe d15(q[15], d[15], clk, enable, reset);
+	dffe d16(q[16], d[16], clk, enable, reset);
+	dffe d17(q[17], d[17], clk, enable, reset);
+	dffe d18(q[18], d[18], clk, enable, reset);
+	dffe d19(q[19], d[19], clk, enable, reset);
+	dffe d20(q[20], d[20], clk, enable, reset);
+	dffe d21(q[21], d[21], clk, enable, reset);
+	dffe d22(q[22], d[22], clk, enable, reset);
+	dffe d23(q[23], d[23], clk, enable, reset);
+	dffe d24(q[24], d[24], clk, enable, reset);
+	dffe d25(q[25], d[25], clk, enable, reset);
+	dffe d26(q[26], d[26], clk, enable, reset);
+	dffe d27(q[27], d[27], clk, enable, reset);
+	dffe d28(q[28], d[28], clk, enable, reset);
+	dffe d29(q[29], d[29], clk, enable, reset);
+	dffe d30(q[30], d[30], clk, enable, reset);
+	dffe d31(q[31], d[31], clk, enable, reset);
 endmodule // register
 
 
 
 // dffe: D-type flip-flop with enable
 //
-// q      (output) - Current value of flip flop
-// d      (input)  - Next value of flip flop
-// clk    (input)  - Clock (positive edge-sensitive)
+// q	  (output) - Current value of flip flop
+// d	  (input)  - Next value of flip flop
+// clk	(input)  - Clock (positive edge-sensitive)
 // enable (input)  - Load new value? (yes = 1, no = 0)
 // reset  (input)  - Asynchronous reset   (reset =  1)
 //
 module dffe(q, d, clk, enable, reset);
    output q;
-   reg    q;
+   reg	q;
    input  d;
    input  clk, enable, reset;
 
    always@(reset)
-     if (reset == 1'b1)
-       q <= 0;
+	 if (reset == 1'b1)
+	   q <= 0;
 
    always@(posedge clk)
-     if ((reset == 1'b0) && (enable == 1'b1))
-       q <= d;
+	 if ((reset == 1'b0) && (enable == 1'b1))
+	   q <= d;
 endmodule // dffe
 
--- Lab3/register_tb.v
+++ Lab3/register_tb.v
@@ -1,35 +1,37 @@
 module test;
-    reg       clk = 0, enable = 0, reset = 1;  // start by reseting the register file
+	reg	   clk = 0, enable = 0, reset = 1;  // start by reseting the register file
 
-    /* Make a regular pulsing clock with a 10 time unit period. */
-    always #5 clk = !clk;
+	/* Make a regular pulsing clock with a 10 time unit period. */
+	always #5 clk = !clk;
 
-    reg [31:0] d;
+	reg [31:0] d;
 
-    wire [31:0] q;
-    initial begin
-        $dumpfile("register.vcd");
-        $dumpvars(0, test);
-        # 10  reset = 0;      // stop reseting the register 
+	wire [31:0] q;
+	initial begin
+		$dumpfile("register.vcd");
+		$dumpvars(0, test);
+		# 10  reset = 0;	  // stop reseting the register 
 
-        # 10
-          // write 88 to the register
-          enable = 1;
-          d = 88;
+		# 10
+		  // write 88 to the register
+		  enable = 1;
+		  d = 88;
 
-        # 10
-          // try writing to the register when its disabled
-          enable = 0;
-          d = 89;
+		# 10
+		  // try writing to the register when its disabled
+		  enable = 0;
+		  d = 89;
 
-        // Add your own testcases here!
+		// Add your own testcases here!
 
-        # 700 $finish;
-    end
-    
-    initial begin
-    end
+		# 700 $finish;
+	end
+	
+	initial begin
+		$display("\t d\t  q clk enable reset");
+		$monitor("%d%d %d %d %d (at time %t)", d, q, clk, enable, reset, $time);
+	end
 
-    register reg1(q, d, clk, enable, reset);
+	register reg1(q, d, clk, enable, reset);
    
 endmodule // test
