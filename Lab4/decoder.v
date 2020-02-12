0cdcd28c3b0c3e0d7a6b7efe23727f1ff6903a8e
Birkhoff <zhiqic2@illinois.edu>
2020-02-12 16:40:44 -0600

    Add decoder

--- Lab4/decoder.v
+++ Lab4/decoder.v
@@ -1,18 +1,42 @@
 // mips_decode: a decoder for MIPS arithmetic instructions
 //
-// rd_src      (output) - should the destination register be rd (0) or rt (1)
+// rd_src	  (output) - should the destination register be rd (0) or rt (1)
 // writeenable (output) - should a new value be captured by the register file
-// alu_src2    (output) - should the 2nd ALU source be a register (0), zero extended immediate or sign extended immediate
-// alu_op      (output) - control signal to be sent to the ALU
-// except      (output) - set to 1 when the opcode/funct combination is unrecognized
-// opcode      (input)  - the opcode field from the instruction
-// funct       (input)  - the function field from the instruction
+// alu_src2	(output) - should the 2nd ALU source be a register (0), zero extended immediate or sign extended immediate
+// alu_op	  (output) - control signal to be sent to the ALU
+// except	  (output) - set to 1 when the opcode/funct combination is unrecognized
+// opcode	  (input)  - the opcode field from the instruction
+// funct	   (input)  - the function field from the instruction
 //
+`define ALU_ADDU   3'h0
+`define ALU_ADD    3'h2
+`define ALU_SUB    3'h3
+`define ALU_AND    3'h4
+`define ALU_OR     3'h5
+`define ALU_NOR    3'h6
+`define ALU_XOR    3'h7
 
 module mips_decode(rd_src, writeenable, alu_src2, alu_op, except, opcode, funct);
-    output       rd_src, writeenable, except;
-    output [1:0] alu_src2;
-    output [2:0] alu_op;
-    input  [5:0] opcode, funct;
+	output rd_src, writeenable, except;
+	output [1:0] alu_src2;
+	output [2:0] alu_op;
+	input [5:0] opcode, funct;
 
+	assign writeenable = opcode == `OP_OTHER0 & funct >= `OP0_ADD & funct <= `OP0_NOR & funct != `OP0_SUBU | opcode == `OP_ADDI | opcode == `OP_ADDIU | opcode == `OP_ANDI | opcode == `OP_ORI | opcode == `OP_XORI;
+
+	assign rd_src = opcode == `OP_ADDI | opcode == `OP_ADDIU | opcode == `OP_ANDI | opcode == `OP_ORI | opcode == `OP_XORI;
+
+	assign except = ~((opcode == `OP_OTHER0 & funct >= `OP0_ADD & funct <= `OP0_NOR & funct != `OP0_SUBU) | opcode == `OP_ADDI | opcode == `OP_ADDIU | opcode == `OP_ANDI | opcode == `OP_ORI | opcode == `OP_XORI);
+
+	assign alu_src2 = (opcode == `OP_OTHER0 & funct >= `OP0_ADD & funct <= `OP0_NOR) ? 2'b00 :
+	(opcode == `OP_ADDI | opcode == `OP_ADDIU) ? 2'b01 :
+	(opcode == `OP_ANDI | opcode == `OP_ORI | opcode == `OP_XORI) ? 2'b10 : 2'b11;
+
+	assign alu_op = (opcode == `OP_ADDI | opcode == `OP_OTHER0 & funct == `OP0_ADD) ? `ALU_ADD :
+	(opcode == `OP_ADDIU | opcode == `OP_OTHER0 & funct == `OP0_ADDU) ? `ALU_ADDU :
+	(opcode == `OP_OTHER0 & funct == `OP0_SUB) ? `ALU_SUB :
+	(opcode == `OP_ANDI | opcode == `OP_OTHER0 & funct == `OP0_AND) ? `ALU_AND :
+	(opcode == `OP_ORI | opcode == `OP_OTHER0 & funct == `OP0_OR) ? `ALU_OR :
+	(opcode == `OP_OTHER0 & funct == `OP0_NOR) ? `ALU_NOR :
+	(opcode == `OP_XORI | opcode == `OP_OTHER0 & funct == `OP0_XOR) ? `ALU_XOR : 3'b001;
 endmodule // mips_decode
--- Lab4/decoder_tb.v
+++ Lab4/decoder_tb.v
@@ -1,21 +1,36 @@
 module decoder_test;
-    reg [5:0] opcode, funct;
+	reg [5:0] opcode, funct;
 
-    initial begin
-        $dumpfile("decoder.vcd");
-        $dumpvars(0, decoder_test);
+	initial begin
+		$dumpfile("decoder.vcd");
+		$dumpvars(0, decoder_test);
 
-             opcode = `OP_OTHER0; funct = `OP0_ADD; // try addition
-        # 10 opcode = `OP_OTHER0; funct = `OP0_SUB; // try subtraction
-        // add more tests here!
+			opcode = `OP_OTHER0; funct = `OP0_ADD;
+		# 10 opcode = `OP_OTHER0; funct = `OP0_ADDU;
+		# 10 opcode = `OP_OTHER0; funct = `OP0_SUB;
+		# 10 opcode = `OP_OTHER0; funct = `OP0_AND;
+		# 10 opcode = `OP_OTHER0; funct = `OP0_OR;
+		# 10 opcode = `OP_OTHER0; funct = `OP0_NOR;
+		# 10 opcode = `OP_OTHER0; funct = `OP0_XOR;
+		# 10 funct = 6'b000000; opcode = `OP_ADDI;
+		# 10 funct = 6'b000000; opcode = `OP_ADDIU;
+		# 10 funct = 6'b000000; opcode = `OP_ANDI;
+		# 10 funct = 6'b000000; opcode = `OP_ORI;
+		# 10 funct = 6'b000000; opcode = `OP_XORI;
+		# 10 funct = `OP0_SUBU; opcode = `OP_OTHER0;
+		# 10 funct = 6'b000000; opcode = `OP_OTHER0;
+		# 10 $finish;
+	end
 
-        # 10 $finish;
-    end
+	// use gtkwave to test correctness
+	wire [2:0] alu_op;
+	wire [1:0] alu_src2; 
+	wire	   rd_src, writeenable, except;
+	mips_decode decoder(rd_src, writeenable, alu_src2, alu_op, except,
+						opcode, funct);
 
-    // use gtkwave to test correctness
-    wire [2:0] alu_op;
-    wire [1:0] alu_src2; 
-    wire       rd_src, writeenable, except;
-    mips_decode decoder(rd_src, writeenable, alu_src2, alu_op, except,
-                        opcode, funct);
+	initial begin
+		$display("opcode funct alu_op rd_src alu_src2 writeenable except");
+		$monitor("%x\t%x\t%x\t%x\t%x\t%x\t%x\t(at time %t)", opcode, funct, alu_op, rd_src, alu_src2, writeenable, except, $time);
+	end
 endmodule // decoder_test
