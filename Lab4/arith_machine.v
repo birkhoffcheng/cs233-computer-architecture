7320d16f55459ec7f61fd281e224b268ce5c1d96
Birkhoff <zhiqic2@illinois.edu>
2020-02-15 23:27:01 -0600

    Add arithmetic machine

--- Lab4/arith_machine.v
+++ Lab4/arith_machine.v
@@ -5,24 +5,36 @@
 // reset  (input)  - set to 1 to set all registers to zero, set to 0 for normal execution.
 
 module arith_machine(except, clock, reset);
-    output      except;
-    input       clock, reset;
+	output except;
+	input clock, reset;
 
-    wire [31:0] inst;  
-    wire [31:0] PC;  
+	wire [31:0] inst;
+	wire [31:0] PC, next_PC;
+	wire [31:0] rsData, rtData, rdData;
+	wire [4:0] rsAddr, rtAddr, rdAddr;
+	wire writeenable, rd_src;
+	wire [31:0] alu_in_b, sign_extended_immediate, zero_extended_immediate;
+	wire [1:0] alu_src2;
+	wire [2:0] alu_op;
 
-    // DO NOT comment out or rename this module
-    // or the test bench will break
-    register #(32) PC_reg( /* connect signals */ );
+	alu32 alu_0(next_PC,,,, PC, 32'h00000004, `ALU_ADD);
 
-    // DO NOT comment out or rename this module
-    // or the test bench will break
-    instruction_memory im( /* connect signals */ );
+	// DO NOT comment out or rename this module
+	// or the test bench will break
+	register #(32) PC_reg(PC, next_PC, clock, 1'b1, reset);
 
-    // DO NOT comment out or rename this module
-    // or the test bench will break
-    regfile rf ( /* connect signals */ );
+	// DO NOT comment out or rename this module
+	// or the test bench will break
+	instruction_memory im(inst, PC[31:2]);
 
-    /* add other modules */
-   
+	// DO NOT comment out or rename this module
+	// or the test bench will break
+	regfile rf(rsData, rtData, inst[25:21], inst[20:16], rdAddr, rdData, writeenable, clock, reset);
+	
+	mux2v #(5) mux_0(rdAddr, inst[15:11], inst[20:16], rd_src);
+	mux3v #(32) mux_1(alu_in_b, rtData, sign_extended_immediate, zero_extended_immediate, alu_src2);
+	alu32 alu_1(rdData,,,, rsData, alu_in_b, alu_op);
+	assign sign_extended_immediate = {{16{inst[15]}}, inst[15:0]};
+	assign zero_extended_immediate = {16'h0000, inst[15:0]};
+	mips_decode mips_decoder(rd_src, writeenable, alu_src2, alu_op, except, inst[31:26], inst[5:0]);
 endmodule // arith_machine
---
+++ Lab4/extender.v
@@ -0,0 +1,21 @@
+module sign_extender(out, in);
+	output [31:0] out;
+	input [15:0] in;
+	assign out[15:0] = in;
+	assign out[16] = in[15];
+	assign out[17] = in[15];
+	assign out[18] = in[15];
+	assign out[19] = in[15];
+	assign out[20] = in[15];
+	assign out[21] = in[15];
+	assign out[22] = in[15];
+	assign out[23] = in[15];
+	assign out[24] = in[15];
+	assign out[25] = in[15];
+	assign out[26] = in[15];
+	assign out[27] = in[15];
+	assign out[28] = in[15];
+	assign out[29] = in[15];
+	assign out[30] = in[15];
+	assign out[31] = in[15];
+endmodule
