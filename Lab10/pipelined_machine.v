409a3c50c92d57a95541511f33928ab5cabaf8cc
Birkhoff <zhiqic2@illinois.edu>
2020-04-05 12:24:21 -0500

    Implemented forwarding

--- Lab10/pipelined_machine.v
+++ Lab10/pipelined_machine.v
@@ -17,7 +17,7 @@ module pipelined_machine(clk, reset);
 
 	wire RegWrite, RegWrite_MW, BEQ, ALUSrc, MemRead, MemRead_MW, MemWrite, MemWrite_MW, MemToReg, MemToReg_MW, RegDst;
 	wire PCSrc, zero;
-	wire [31:0] rd1_data, rd2_data, rd2_data_MW, B_data, alu_out_data, alu_out_data_DE, load_data, wr_data;
+	wire [31:0] rd1_data, rd1_data_forwarded, rd2_data, rd2_data_forwarded, rd2_data_MW, B_data, alu_out_data, alu_out_data_DE, load_data, wr_data;
 
 
 	// DO NOT comment out or rename this module
@@ -35,22 +35,20 @@ module pipelined_machine(clk, reset);
 	// or the test bench will break
 	instruction_memory imem(inst_IF, PC[31:2]);
 
-	mips_decode decode(ALUOp, RegWrite, BEQ, ALUSrc, MemRead, MemWrite, MemToReg, RegDst,
-		opcode, funct);
+	mips_decode decode(ALUOp, RegWrite, BEQ, ALUSrc, MemRead, MemWrite, MemToReg, RegDst, opcode, funct);
 
 	// DO NOT comment out or rename this module
 	// or the test bench will break
-	regfile rf (rd1_data, rd2_data,
-			   rs, rt, wr_regnum_MW, wr_data,
-			   RegWrite_MW, clk, reset);
+	regfile rf (rd1_data, rd2_data, rs, rt, wr_regnum_MW, wr_data, RegWrite_MW, clk, reset);
 
-	mux2v #(32) imm_mux(B_data, rd2_data, imm, ALUSrc);
-	alu32 alu(alu_out_data_DE, zero, ALUOp, rd1_data, B_data);
+	mux2v #(32) imm_mux(B_data, rd2_data_forwarded, imm, ALUSrc);
+	alu32 alu(alu_out_data_DE, zero, ALUOp, rd1_data_forwarded, B_data);
 	register #(32) alu_out_pipeline_reg(alu_out_data, alu_out_data_DE, clk, 1'b1, reset);
+
 	// DO NOT comment out or rename this module
 	// or the test bench will break
 	data_mem data_memory(load_data, alu_out_data, rd2_data_MW, MemRead_MW, MemWrite_MW, clk, reset);
-	register #(32) rd2_data_pipeline_reg(rd2_data_MW, rd2_data, clk, 1'b1, reset);
+	register #(32) rd2_data_pipeline_reg(rd2_data_MW, rd2_data_forwarded, clk, 1'b1, reset);
 	register #(1) MemRead_pipeline_reg(MemRead_MW, MemRead, clk, 1'b1, reset);
 	register #(1) MemWrite_pipeline_reg(MemWrite_MW, MemWrite, clk, 1'b1, reset);
 	register #(1) MemToReg_pipeline_reg(MemToReg_MW, MemToReg, clk, 1'b1, reset);
@@ -59,4 +57,6 @@ module pipelined_machine(clk, reset);
 	mux2v #(32) wb_mux(wr_data, alu_out_data, load_data, MemToReg_MW);
 	mux2v #(5) rd_mux(wr_regnum, rt, rd, RegDst);
 
+	assign rd1_data_forwarded = (RegWrite_MW & rs == wr_regnum_MW & rs != 5'b0) ? alu_out_data : rd1_data;
+	assign rd2_data_forwarded = (RegWrite_MW & rt == wr_regnum_MW & rt != 5'b0) ? alu_out_data : rd2_data;
 endmodule // pipelined_machine
