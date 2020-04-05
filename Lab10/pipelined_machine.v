bb36ef445a02b08a38e2e8cc65906c8a185b7858
Birkhoff <zhiqic2@illinois.edu>
2020-04-05 14:35:34 -0500

    Implemented stalling

--- Lab10/pipelined_machine.v
+++ Lab10/pipelined_machine.v
@@ -4,7 +4,6 @@ module pipelined_machine(clk, reset);
 	wire [31:0] PC;
 	wire [31:2] next_PC, PC_plus4, PC_target, PC_plus4_DE;
 	wire [31:0] inst, inst_IF;
-	register #(32) inst_pipeline_reg(inst, inst_IF, clk, 1'b1, reset);
 	wire [31:0] imm = {{ 16{inst[15]} }, inst[15:0] };  // sign-extended immediate
 	wire [4:0] rs = inst[25:21];
 	wire [4:0] rt = inst[20:16];
@@ -18,15 +17,15 @@ module pipelined_machine(clk, reset);
 	wire RegWrite, RegWrite_MW, BEQ, ALUSrc, MemRead, MemRead_MW, MemWrite, MemWrite_MW, MemToReg, MemToReg_MW, RegDst;
 	wire PCSrc, zero;
 	wire [31:0] rd1_data, rd1_data_forwarded, rd2_data, rd2_data_forwarded, rd2_data_MW, B_data, alu_out_data, alu_out_data_DE, load_data, wr_data;
-
+	wire stall = MemRead_MW & RegDst & (rs == wr_regnum_MW & rs != 5'b0) | rt == wr_regnum_MW & rt != 5'b0;
 
 	// DO NOT comment out or rename this module
 	// or the test bench will break
-	register #(30, 30'h100000) PC_reg(PC[31:2], next_PC[31:2], clk, /* enable */1'b1, reset);
+	register #(30, 30'h100000) PC_reg(PC[31:2], next_PC[31:2], clk, ~stall, reset);
 
 	assign PC[1:0] = 2'b0;  // bottom bits hard coded to 00
 	adder30 next_PC_adder(PC_plus4, PC[31:2], 30'h1);
-	register #(30) pc_plus4_pipeline_reg(PC_plus4_DE, PC_plus4, clk, 1'b1, reset);
+	register #(30) pc_plus4_pipeline_reg(PC_plus4_DE, PC_plus4, clk, ~stall, reset);
 	adder30 target_PC_adder(PC_target, PC_plus4_DE, imm[29:0]);
 	mux2v #(30) branch_mux(next_PC, PC_plus4, PC_target, PCSrc);
 	assign PCSrc = BEQ & zero;
@@ -34,12 +33,12 @@ module pipelined_machine(clk, reset);
 	// DO NOT comment out or rename this module
 	// or the test bench will break
 	instruction_memory imem(inst_IF, PC[31:2]);
-
+	register #(32) inst_pipeline_reg(inst, inst_IF, clk, ~stall, reset);
 	mips_decode decode(ALUOp, RegWrite, BEQ, ALUSrc, MemRead, MemWrite, MemToReg, RegDst, opcode, funct);
 
 	// DO NOT comment out or rename this module
 	// or the test bench will break
-	regfile rf (rd1_data, rd2_data, rs, rt, wr_regnum_MW, wr_data, RegWrite_MW, clk, reset);
+	regfile rf(rd1_data, rd2_data, rs, rt, wr_regnum_MW, wr_data, RegWrite_MW, clk, reset);
 
 	mux2v #(32) imm_mux(B_data, rd2_data_forwarded, imm, ALUSrc);
 	alu32 alu(alu_out_data_DE, zero, ALUOp, rd1_data_forwarded, B_data);
