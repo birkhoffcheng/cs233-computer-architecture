6d2907dcbe7cd8c8a73d55cd7ffa59cb71a7c744
Birkhoff <zhiqic2@illinois.edu>
2020-04-15 21:12:15 -0500

    Add tiling to transpose

--- Lab12/transpose.cpp
+++ Lab12/transpose.cpp
@@ -7,13 +7,15 @@
 // will be useful
 // remember that you shouldn't go over SIZE
 using std::min;
-
+#define TILE_SIZE 32
 // modify this function to add tiling
 void
 transpose_tiled(int **src, int **dest) {
-    for (int i = 0; i < SIZE; i ++) {
-        for (int j = 0; j < SIZE; j ++) {
-            dest[i][j] = src[j][i];
-        }
-    }
+	for (int j = 0; j < SIZE; j += TILE_SIZE) {
+		for (int i = 0; i < SIZE; i++) {
+			for (int jj = j; jj < min(j + TILE_SIZE, SIZE); jj++) {
+				dest[i][jj] = src[jj][i];
+			}
+		}
+	}
 }
